e5199cfa187aa2c7285d42b5b5afef3d0925c7f4
root <root@computob3>
2016-03-12 09:26:33 -0600

    Parcial Progra II

---
+++ parcial.php
@@ -0,0 +1,11 @@
+<?php
+$num=readline("Ingrese un valor: ");
+$if($num%2){
+	echo "este numero es par".$num."\n";
+}else{
+	echo "este numero es impar: ".$num."\n"
+
+}
+Wilber Fernando Ramos Gómez
+RG0132983
+?>
